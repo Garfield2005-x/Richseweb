ca0615a216f896d37c95af9894664ab9a6b27e38
Boxx <boxx3345@gmail.com>
2026-02-25 15:23:58 +0700

    fix: remove prisma v7 config

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import nodemailer from "nodemailer";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma"; // 🔥 ต้องมีไฟล์นี้
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
