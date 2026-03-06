a97883f08645d930e6f6716f2e7c9b9e729c46c2
Boxx <boxx3345@gmail.com>
2026-03-06 17:33:45 +0700

    update auth system and prisma schema

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import nodemailer from "nodemailer";
 import { UAParser } from "ua-parser-js";
 import { headers } from "next/headers";
